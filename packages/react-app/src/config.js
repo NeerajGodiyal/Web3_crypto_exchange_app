ddf0887f3d9830250b9ab667669c8b1e275eef67
Neeraj <neerajgodiyal250@gmail.com>
2023-01-21 06:13:52 +0530

    Update config.js

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "0x3bF533Dd04abBF4eDE45F353764102aB66359557"; 
+export const ROUTER_ADDRESS = "ur contract address"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/WUpfkNHSLy9pFj-Lo6FpyA_8KiLfIigq",
+    [Goerli.chainId]: "ur goerli url",
   },
 };
