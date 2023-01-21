6d951ccdb4ff1b1dd4266c010c26d934e979ce0f
Neeraj <neerajgodiyal250@gmail.com>
2023-01-21 06:12:31 +0530

    first commit

---
+++ packages/react-app/src/assets/index.js
@@ -0,0 +1,7 @@
+import chevronDown from "./chevron-down.svg";
+import ethereumLogo from "./ethereumLogo.png";
+import uniswapLogo from "./uniswapLogo.png";
+import indian from "./indian.svg";
+
+
+export { chevronDown, ethereumLogo, uniswapLogo , indian };
